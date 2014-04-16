3cdfc5ccc815da6918549dc86b63fee33a524596
Peter <peterhsaxton@gmail.com>
2014-04-16 15:54:31 +0100

    Added code to return width

--- spec/viewframe_spec.js
+++ spec/viewframe_spec.js
@@ -4,8 +4,14 @@ describe("Viewframe", function () {
     size = new Point (800, 600);
     frame = new viewFrame (origin, size);
   });
-  
+
   it('should be initialized with a size and position', function () {
     expect(frame.getOrigin()).toBe(origin);
+  });
+
+  describe('Dimensions', function () {
+    it('should be possible to read the width', function () {
+      expect(frame.getWidth()).toEqual(800);
+    });
   });
 });
--- src/viewframe.js
+++ src/viewframe.js
@@ -2,4 +2,7 @@ function viewFrame (origin, size) {
   this.getOrigin = function () {
     return origin;
   };
+  this.getWidth = function () {
+    return size.x;
+  };
 }
