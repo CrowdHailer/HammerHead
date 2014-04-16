d2e3b877c70361f329b0a49ecb3b608c38ceadf0
Peter <peterhsaxton@gmail.com>
2014-04-16 18:36:00 +0100

    Added shallow translation dummy function

--- spec/point_spec.js
+++ spec/point_spec.js
@@ -4,4 +4,11 @@ describe('Point', function(){
     expect(point.x).toBe(2);
     expect(point.y).toBe(3);
   });
+
+  it('should be able to add points', function () {
+    var point1 = new Point(2,3);
+    var point2 = new Point(3,-1);
+    console.log(point1 + point2)
+
+  });
 });
