50e73de237f29e76d12d545353fabd36d957a98b
Peter <peterhsaxton@gmail.com>
2014-04-16 15:49:46 +0100

    DRY by collecting viewFrame setup

--- spec/viewframe_spec.js
+++ spec/viewframe_spec.js
@@ -1,8 +1,11 @@
 describe("Viewframe", function () {
-  it('should be initialized with a size and position', function () {
+  beforeEach(function () {
     origin = new Point (0, 0);
     size = new Point (800, 600);
     frame = new viewFrame (origin, size);
+  });
+  
+  it('should be initialized with a size and position', function () {
     expect(frame.getOrigin()).toBe(origin);
   });
 });
