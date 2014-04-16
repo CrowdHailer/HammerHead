c5ac8f0c5b4099e8b313866ac1378de007c63b37
Peter <peterhsaxton@gmail.com>
2014-04-16 16:31:22 +0100

    Modified tests to read dimensions

--- spec/viewframe_spec.js
+++ spec/viewframe_spec.js
@@ -3,6 +3,10 @@ describe("Viewframe", function () {
     origin = new Point (0, 0);
     size = new Point (800, 600);
     frame = new viewFrame (origin, size);
+    dummy = {
+      Width: 800,
+      Height: 600
+    };
   });
 
   it('should be initialized with a size and position', function () {
@@ -13,5 +17,11 @@ describe("Viewframe", function () {
     it('should be possible to read the width', function () {
       expect(frame.getWidth()).toEqual(800);
     });
+
+    ['Height', 'Width'].forEach(function (dimension) {
+      it('should have a '+dimension, function(){
+        expect(frame['get'+dimension]()).toEqual(dummy[dimension]);
+      });
+    }); 
   });
 });
--- src/viewframe.js
+++ src/viewframe.js
@@ -2,7 +2,12 @@ function viewFrame (origin, size) {
   this.getOrigin = function () {
     return origin;
   };
+
   this.getWidth = function () {
     return size.x;
+  };
+
+  this.getHeight = function () {
+    return size.y;
   };
 }
