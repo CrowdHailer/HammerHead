319384033ac49e11ea58527b2d0f04e5078e5e92
Peter <peterhsaxton@gmail.com>
2014-04-18 09:14:22 +0100

    Changed pattern again

--- src/viewframe.js
+++ src/viewframe.js
@@ -1,38 +1,32 @@
 function viewFrame (origin, size) {
-  this.origin = origin;
-  this.size = size;
-}
-
-viewFrame.prototype = {
-  constructor: viewFrame,
-  width: function () {
-    return this.size.x;
-  },
-  height: function () {
-    return this.size.y;
-  },
-  left: function () {
-    return this.origin.x;
-  },
-  bottom: function () {
-    return this.origin.y;
-  },
-  toString: function () {
+  this.width = function () {
+    return size.x;
+  };
+  this.height = function () {
+    return size.y;
+  };
+  this.left = function () {
+    return origin.x;
+  };
+  this.bottom = function () {
+    return origin.y;
+  };
+  this.toString = function () {
     return [this.left(), this.bottom(), this.width(), this.height()].join(' ');
-  },
-  translate: function (vector) {
-    return new viewFrame(this.origin.add(vector), this.size);
-  },
-  anchorTranslate: function (vector) {
-    this.origin = this.origin.add(vector);
-  },
-  scaleAt: function (center, magnification) {
-    finalOrigin = this.origin.subtract(center).multiply(magnification).add(center);
-    finalSize = this.size.multiply(magnification);
+  };
+  this.translate = function (vector) {
+    return new viewFrame(origin.add(vector), size);
+  };
+  this.anchorTranslate = function (vector) {
+    origin = origin.add(vector);
+  };
+  this.scaleAt = function (center, magnification) {
+    finalOrigin = origin.subtract(center).multiply(magnification).add(center);
+    finalSize = size.multiply(magnification);
     return new viewFrame(finalOrigin, finalSize);
-  },
-  anchorScale: function (center, magnification) {
-    this.origin = this.origin.subtract(center).multiply(magnification).add(center);
-    this.size = this.size.multiply(magnification);
-  }
-};
+  };
+  this.anchorScale = function (center, magnification) {
+    origin = origin.subtract(center).multiply(magnification).add(center);
+    size = size.multiply(magnification);
+  };
+}
