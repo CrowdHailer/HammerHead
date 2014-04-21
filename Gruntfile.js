0d098af05c11ff907528c557ea2ff9f37e5f76a0
Peter <peterhsaxton@gmail.com>
2014-04-21 23:20:07 +0100

    Created grunt tasks to concat and uglify

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: '\n'
+      },
+      dist: {
+        src: [
+          'src/point.js',
+          'src/viewframe.js',
+          'src/viewer.js'
+        ],
+        dest: '<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
