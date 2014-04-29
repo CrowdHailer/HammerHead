2a6ef2de4a948f1b1d731f980816b4af33ea77b5
Peter <peterhsaxton@gmail.com>
2014-04-29 08:56:37 +0100

    wrapped viewFrame namespace

--- dist/hammerhead.js
+++ dist/hammerhead.js
@@ -31,48 +31,49 @@ Point.prototype = {
     return this.scaleTransform(matrix);
   }
 };
-function viewFrame (origin, size) {
-  var HOME = { origin: origin, size: size };
+var viewFrame;
+(function(){
+  viewFrame = function(origin, size) {
+    var HOME = { origin: origin, size: size };
 
-  this.dX = function () { return size.x;   };
-  this.dY = function () { return size.y;   };
-  this.x0 = function () { return origin.x; };
-  this.y0 = function () { return origin.y; };
+    this.dX = function () { return size.x;   };
+    this.dY = function () { return size.y;   };
+    this.x0 = function () { return origin.x; };
+    this.y0 = function () { return origin.y; };
 
-  this.toString = function () {
-    return [this.x0(), this.y0(), this.dX(), this.dY()].join(' ');
+    this.toString = function () {
+      return [this.x0(), this.y0(), this.dX(), this.dY()].join(' ');
+    };
+
+    this.translate = function (vector) {
+      return new viewFrame(origin.add(vector), size);
+    };
+
+    this.scale = function (center, magnification) {
+      newOrigin = origin.subtract(center).multiply(magnification).add(center);
+      newSize = size.multiply(magnification);
+      return new viewFrame(newOrigin, newSize);
+    };
+
+    this.home = function (destination) {
+      var target = destination || HOME;
+      origin = target.origin;
+      size = target.size;
+    };
+
+    this.anchor = {
+
+      translate: function (vector) {
+        origin = origin.add(vector);
+      },
+
+      scale: function (center, magnification) {
+        origin = origin.subtract(center).multiply(magnification).add(center);
+        size = size.multiply(magnification);
+      }
+    };
   };
-
-  this.translate = function (vector) {
-    return new viewFrame(origin.add(vector), size);
-  };
-
-  this.scale = function (center, magnification) {
-    newOrigin = origin.subtract(center).multiply(magnification).add(center);
-    newSize = size.multiply(magnification);
-    return new viewFrame(newOrigin, newSize);
-  };
-
-  this.home = function (destination) {
-    var target = destination || HOME;
-    origin = target.origin;
-    size = target.size;
-  };
-
-  this.anchor = {
-
-    translate: function (vector) {
-      origin = origin.add(vector);
-    },
-
-    scale: function (center, magnification) {
-      origin = origin.subtract(center).multiply(magnification).add(center);
-      size = size.multiply(magnification);
-    }
-
-  };
-
-}
+}());
 
 (function (){
 
