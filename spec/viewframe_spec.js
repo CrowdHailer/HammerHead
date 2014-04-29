1e936af35d3db54e55fa5f0ed359c11b2ff42ddb
Peter <peterhsaxton@gmail.com>
2014-04-29 10:38:59 +0100

    Refaced viewFrame tests

--- spec/viewframe_spec.js
+++ spec/viewframe_spec.js
@@ -1,100 +1,76 @@
 describe("Viewframe", function () {
-  describe('reading from element', function(){
+  var element, origin, size;
+  beforeEach(function(){
+    element = {
+      getAttribute: function(){ return '1 2 3 4'; }
+    };
+    origin = new Point (0, 1);
+    size = new Point (8, 6);
+  });
+
+  describe('initialising the viewFrame', function(){
     it('should read the viewbox charachteristics when not given specifics', function(){
-      var element = {
-        getAttribute: function(){
-          return '100 200 400 400';
-        }
-      };
       var frame = new viewFrame(element);
-      expect(frame.x0()).toEqual(100);
-      expect(frame.y0()).toEqual(200);
-      expect(frame.dX()).toEqual(400);
-      expect(frame.dX()).toEqual(400);
+      expect(frame.x0()).toEqual(1);
+      expect(frame.y0()).toEqual(2);
+      expect(frame.dX()).toEqual(3);
+      expect(frame.dY()).toEqual(4);
+    });
+
+    it('should use origin and size arguments if provided', function(){
+      frame = new viewFrame (element, origin, size);
+      expect(frame.x0()).toEqual(0);
+      expect(frame.y0()).toEqual(1);
+      expect(frame.dX()).toEqual(8);
+      expect(frame.dY()).toEqual(6);
+    });
+
+    it('should return a viewBox string', function(){
+      frame = new viewFrame (element, origin, size);
+      expect(frame.toString()).toBe('0 1 8 6');
     });
   });
 
-  describe('viewfram given explicit parameter', function(){
-    var origin, size, frame, SVGElement;
+  describe('transformations in SVG pixels', function(){
     beforeEach(function () {
-      origin = new Point (0, 100);
-      size = new Point (800, 600);
-      frame = new viewFrame (SVGElement, origin, size);
+      frame = new viewFrame (element, origin, size);
     });
 
-    describe('Dimensions', function () {
-      
-      [['dY',600], ['dX',800], ['x0',0], ['y0', 100]].forEach(function (dimension) {
-        it('should have a '+dimension[0], function(){
-          expect(frame[dimension[0]]()).toEqual(dimension[1]);
-        });
-      });
-
-      it('should be able to give an attribute string', function (){
-        expect(frame.toString()).toBe('0 100 800 600');
-      });
+    it('should translate temporarily when permanent argument untrue', function(){
+      expect(frame.translate(new Point(1, 1)).toString()).toEqual('1 2 8 6');
+      expect(frame.toString()).toEqual('0 1 8 6');
     });
 
-    describe('shallow transform', function () {
-      it('should be able to translate', function () {
-        var translation = new Point (100, 100);
-        expect(frame.translate(translation).toString()).toEqual('100 200 800 600');
-        expect(frame.toString()).toEqual('0 100 800 600');
-      });
-
-      it('should translate from the anchor point', function () {
-        var translation1 = new Point (100, 100);
-        var translation2 = new Point (200, -100);
-        frame.translate(translation1);
-        expect(frame.translate(translation2).toString()).toEqual('200 0 800 600');
-      });
-
-      it('should scale to a point', function () {
-        var point = new Point (0, 100);
-        expect(frame.scale(point, 0.5).toString()).toEqual('0 100 400 300');
-      });
-      it('should scale to a point', function () {
-        var point = new Point (800, 700);
-        expect(frame.scale(point, 0.5).toString()).toEqual('400 400 400 300');
-      });
+    it('should translate permanently when permanent argument true', function(){
+      frame.translate(new Point(1, 1), true);
+      expect(frame.toString()).toEqual('1 2 8 6');
     });
 
-    describe('deep transform', function () {
-      it('should be able to translate permanently', function () {
-        var translation = new Point (100, 100);
-        frame.translate(translation, true);
-        expect(frame.x0()).toEqual(100);
-        expect(frame.y0()).toEqual(200);
-      });
-
-      it('should multitranslate', function () {
-        var translation1 = new Point (100, 100);
-        var translation2 = new Point (200, -100);
-        frame.translate(translation1, true);
-        frame.translate(translation2, true);
-        expect(frame.x0()).toEqual(300);
-        expect(frame.y0()).toEqual(100);
-      });
-
-      it('should be able to scale Permanently', function () {
-        var center = new Point (0, 100);
-        frame.scale(center, 2, true);
-        expect(frame.toString()).toEqual('0 100 1600 1200');
-      });
+    it('should scale temporarily when permanent argument untrue', function(){
+      expect(frame.scale(new Point(0, 1), 0.5).toString()).toEqual('0 1 4 3');
+      expect(frame.toString()).toEqual('0 1 8 6');
     });
 
-    it('should have a permanent home value', function () {
-      var translation = new Point (100, 100);
-      frame.translate(translation, true);
+    it('should scale permanently when permanent argument true', function(){
+      frame.scale(new Point(0, 1), 0.5, true);
+      expect(frame.toString()).toEqual('0 1 4 3');
+    });
+  });
+
+  describe('moving to a target view', function(){
+    beforeEach(function () {
+      frame = new viewFrame (element, origin, size);
+    });
+
+    it('should home to its initial values if no argument given', function(){
+      frame.translate(new Point(1, 1), true);
       frame.home();
-      expect(frame.toString()).toEqual('0 100 800 600');
+      expect(frame.toString()).toEqual('0 1 8 6');
     });
 
-    it('should be able to home to a point', function () {
-      var location = new Point (-100, 200);
-      var extent = new Point (400, 400);
-      frame.home({origin: location, size: extent});
-      expect(frame.toString()).toEqual('-100 200 400 400');
+    it('should set viewbox values when they are provided', function(){
+      frame.home({origin: new Point(-1, 2), size: new Point(4, 5)});
+      expect(frame.toString()).toEqual('-1 2 4 5');
     });
   });
 });
