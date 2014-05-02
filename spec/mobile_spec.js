539928d6d469230ce25d42cb8e04419bd28617d9
Peter <peterhsaxton@gmail.com>
2014-05-02 12:03:19 +0100

    Added temporary transform

--- spec/mobile_spec.js
+++ spec/mobile_spec.js
@@ -6,11 +6,18 @@ describe('Mobile SVG', function(){
       setAttribute: function(){}
     };
     spyOn(element, 'getAttribute').andReturn('0 1 8 6');
+    spyOn(element, 'setAttribute');
     mobileSVG = new MobileSVG(element);
   });
   it('should read the viewbox attribute when not given specifics', function(){
     expect(element.getAttribute).toHaveBeenCalledWith('viewBox');
     expect(mobileSVG._test.viewBox.x0()).toEqual(0);
+  });
+
+  it('should translate in SVG units', function(){
+    delta = new Point(1, 1);
+    mobileSVG.translate(delta);
+    expect(element.setAttribute).toHaveBeenCalledWith('viewBox', '-1 0 8 6');
   });
 
 });
--- src/mobile.js
+++ src/mobile.js
@@ -1,8 +1,14 @@
 var MobileSVG;
 (function(){
   MobileSVG = function(element){
+    var temporaryViewBox;
     var viewBoxString = element.getAttribute('viewBox');
     var viewBox = ViewBox.fromString(viewBoxString);
+
+    this.translate = function(delta){
+      temporaryViewBox = viewBox.translate(delta);
+      element.setAttribute('viewBox', temporaryViewBox.toString());
+    };
 
     this._test = {
       viewBox: viewBox
