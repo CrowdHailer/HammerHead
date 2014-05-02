f749e26d13bd5a3c254018db74014dc65087a225
Peter <peterhsaxton@gmail.com>
2014-05-02 09:11:44 +0100

    Added pinch handlers

--- src/viewer.js
+++ src/viewer.js
@@ -12,7 +12,7 @@ var Hammerhead;
   }
 
   Hammerhead = function (id) {
-    var scaling, lastFrame;
+    // var scaling, lastFrame;
     var element = getSVG(id);
     var viewFrame = new ViewFrame(element);
     var hammertime = Hammer(document, {preventDefault: true}).on('touch', touchHandler);
@@ -23,58 +23,28 @@ var Hammerhead;
       },
       dragend: function(gesture){
         viewFrame.drag(new Point(gesture.deltaX, gesture.deltaY), true);
+      },
+      pinch: function(gesture){
+        viewFrame.zoom(new Point(gesture.center.pageX, gesture.center.pageY), 1.0/gesture.scale);
+      },
+      transformend: function(gesture){
+        viewFrame.zoom(new Point(gesture.center.pageX, gesture.center.pageY), 1.0/event.gesture.scale, true);
       }
     };
 
     var gestureHandler = function(event){
       var gesture = event.gesture;
       gesture.preventDefault();
-      //timecheck here
-      console.log(event.type);
       var handler = handlers[event.type];
       if (handler) { handler(gesture); }
     };
 
-    // dragHandler = function (event) {
-    //   event.gesture.preventDefault();
-    //   viewFrame.drag(new Point(event.gesture.deltaX, event.gesture.deltaY));
-    // };
-
-    // dragendHandler = function (event) {
-    //   event.gesture.preventDefault();
-    //   viewFrame.drag(new Point(event.gesture.deltaX, event.gesture.deltaY), true);
-    // };
-
-    // dragstartHandler = function (event) {
-    //   event.gesture.preventDefault();
-    // };
-
-    pinchHandler = function (event) {
-      event.gesture.preventDefault();
-      lastFrame = viewFrame.zoom(new Point(event.gesture.center.pageX, event.gesture.center.pageY), 1.0/event.gesture.scale);
-      scaling = true;
-    };
-
-    transformendHandler = function (event) {
-      event.gesture.preventDefault();
-      viewFrame.zoom(new Point(event.gesture.center.pageX, event.gesture.center.pageY), 1.0/event.gesture.scale, true);
-      scaling = false;
-    };
-
     function activityOn(instance){
-      instance.on('dragstart drag dragend', gestureHandler);
-      // instance.on('dragstart', dragstartHandler);
-      // instance.on('dragend', dragendHandler);
-      instance.on('pinch', pinchHandler);
-      instance.on('transformend', transformendHandler);
+      instance.on('dragstart drag dragend pinch transformend', gestureHandler);
       instance.on('release', releaseHandler);
     }
     function activityOff(instance){
-      instance.off('dragstart drag dragend', gestureHandler);
-      // instance.off('dragstart', dragstartHandler);
-      // instance.off('dragend', dragendHandler);
-      instance.off('pinch', pinchHandler);
-      instance.off('transformend', transformendHandler);
+      instance.off('dragstart drag dragend pinch transformend', gestureHandler);
       instance.off('release', releaseHandler);
     }
     function touchHandler (event) {
@@ -82,7 +52,6 @@ var Hammerhead;
       if (event.target.ownerSVGElement === element) { activityOn(hammertime); }  
     }
     function releaseHandler (event) {
-      if (scaling) { alert('boo'); };
       activityOff(hammertime);  
     }
     /* test-code */
