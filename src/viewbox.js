c9287d6a2ffc2c4d496dc0ee6fc5e38ecb434f4b
Peter <peterhsaxton@gmail.com>
2014-05-02 10:39:22 +0100

    Started prototype

--- src/viewbox.js
+++ src/viewbox.js
@@ -6,12 +6,13 @@ var ViewBox;
 
     this.x1 = function(){ return maximal.x; };
     this.y1 = function(){ return maximal.y; };
-
-    this.dX = function(){ return this.x1() - this.x0(); };
-    this.dY = function(){ return this.y1() - this.y0(); };
-
-    this.toString = function(){
+  };
+  
+  ViewBox.prototype = {
+    dX: function(){ return this.x1() - this.x0(); },
+    dY: function(){ return this.y1() - this.y0(); },
+    toString: function(){
       return [this.x0(), this.y0(), this.dX(), this.dY()].join(' ');
-    };
+    }
   };
 }());
