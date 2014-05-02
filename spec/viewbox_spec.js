b47d2f4b24ffb83b08fc98e8094c8ab4af4a23e3
Peter <peterhsaxton@gmail.com>
2014-05-02 10:56:13 +0100

    Added translate functionality

--- spec/viewbox_spec.js
+++ spec/viewbox_spec.js
@@ -1,36 +1,42 @@
 describe('ViewBox', function(){
-  var minimal, maximal, viewbox;
+  var minimal, maximal, viewBox;
   beforeEach(function(){
     minimal = new Point(0, 1);
     maximal = new Point(8, 7);
-    viewbox = new ViewBox(minimal, maximal);
+    viewBox = new ViewBox(minimal, maximal);
   });
 
   it('should give a x0 value', function(){
-    expect(viewbox.x0()).toEqual(0);
+    expect(viewBox.x0()).toEqual(0);
   });
 
   it('should give a y0 value', function(){
-    expect(viewbox.y0()).toEqual(1);
+    expect(viewBox.y0()).toEqual(1);
   });
 
   it('should give a x1 value', function(){
-    expect(viewbox.x1()).toEqual(8);
+    expect(viewBox.x1()).toEqual(8);
   });
 
   it('should give a y1 value', function(){
-    expect(viewbox.y1()).toEqual(7);
+    expect(viewBox.y1()).toEqual(7);
   });
 
   it('should give a dX value', function(){
-    expect(viewbox.dX()).toEqual(8);
+    expect(viewBox.dX()).toEqual(8);
   });
 
   it('should give a dY value', function(){
-    expect(viewbox.dY()).toEqual(6);
+    expect(viewBox.dY()).toEqual(6);
   });
 
   it('should return a viewBox string', function(){
-    expect(viewbox.toString()).toEqual('0 1 8 6');
+    expect(viewBox.toString()).toEqual('0 1 8 6');
+  });
+
+  it('should translate', function(){
+    delta = new Point(1, 1);
+    newViewBox = viewBox.translate(delta);
+    expect(newViewBox.toString()).toEqual('-1 0 8 6');
   });
 });
--- src/viewbox.js
+++ src/viewbox.js
@@ -6,13 +6,22 @@ var ViewBox;
 
     this.x1 = function(){ return maximal.x; };
     this.y1 = function(){ return maximal.y; };
+
+    this.getMinimal = function(){ return minimal; };
+    this.getMaximal = function(){ return maximal; };
   };
-  
+
   ViewBox.prototype = {
+    constructor: ViewBox,
     dX: function(){ return this.x1() - this.x0(); },
     dY: function(){ return this.y1() - this.y0(); },
     toString: function(){
       return [this.x0(), this.y0(), this.dX(), this.dY()].join(' ');
+    },
+    translate: function(delta){
+      var newMinimal = this.getMinimal().subtract(delta);
+      var newMaximal = this.getMaximal().subtract(delta);
+      return new this.constructor(newMinimal, newMaximal);
     }
   };
 }());
