7c9947fcfa9bc7274b5a040fd7c41f6a593f016e
Peter <peterhsaxton@gmail.com>
2014-05-11 19:04:05 +0100

    cleaning

--- src/mobile.js
+++ src/mobile.js
@@ -85,13 +85,6 @@
   parent.MobileSVG = create;
 }(Hammerhead));
 
-// var MobileSVG;
-// (function(){
-//   MobileSVG = function(element){
-//     var temporaryViewBox, inverseScreenCTM, viewBox, HOME;
-//     viewBox = ViewBox.fromString(element.getAttribute('viewBox'));
-//     HOME = viewBox;
-//     temporaryViewBox = viewBox;
 //     var getInverseScreenCTM = function(){
 //       var inverse = element.getScreenCTM().inverse();
 //       // Windows Phone hack
@@ -99,41 +92,4 @@
 //       return inverse;
 //     };
 
-//     this.updateCTM = function(){
-//       inverseScreenCTM = getInverseScreenCTM();
-//     };
-//     this.updateCTM();
 
-//     this.translate = function(delta){
-//       temporaryViewBox = viewBox.translate(delta);
-//       element.setAttribute('viewBox', temporaryViewBox.toString());
-//       return this;
-//     };
-
-//     this.drag = function(screenDelta){
-//       var delta = screenDelta.scaleTransform(inverseScreenCTM);
-//       return this.translate(delta);
-//     };
-
-//     this.scale = function(center, magnfication){
-//       temporaryViewBox = viewBox.scale(center, magnfication);
-//       element.setAttribute('viewBox', temporaryViewBox.toString());
-//       return this;
-//     };
-
-//     this.zoom = function(screenCenter, magnfication){
-//       var center = screenCenter.transform(inverseScreenCTM);
-//       return this.scale(center, magnfication);
-//     };
-
-//     this.fix = function(){
-//       viewBox = temporaryViewBox;
-//       return this;
-//     };
-
-//     this.home = function(){
-//       viewBox = HOME;
-//       element.setAttribute('viewBox', viewBox.toString());
-//     };
-//   };
-// }());
